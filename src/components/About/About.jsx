699b59d7e6f345f99caadc1c605ca5d50199d6a7
therudra04 <therudra04@gmail.com>
2026-07-29 16:50:04 +0530

    Modified some files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css';
 import AboutImg from '../../Assets/about.png';
-import CV from "../../Assets/Rudra's CV.pdf";
+import CV from "../../Assets/RudraModi CV.pdf";
 import Info from './Info';
 
 const About = () => {
